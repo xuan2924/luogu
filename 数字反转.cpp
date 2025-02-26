e4a8e74bf8474085c0701a70c55ead937da685b2
xuan <xuan12138@163.com>
2025-02-26 17:42:50 +0800

    添加项目文件。

---
+++ 数字反转.cpp
@@ -0,0 +1,18 @@
+//#include<iostream>
+//#include<string>
+//#include<algorithm>
+//using namespace std;
+//int main() {
+//	string a;
+//	cin >> a;
+//	reverse(a.begin(), a.end());
+//	cout << a;
+//	return 0;
+//
+//	/*string a;
+//	cin >> a;
+//	for (int i = a.size() - 1; i >= 0; i--)
+//		cout << a[i];
+//	return 0;*/
+//
+//}
